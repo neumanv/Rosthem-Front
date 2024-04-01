103deafc176b598da86ae81f5a498d084d6b629f
neumanv <guillermoanv25@gmail.com>
2024-04-01 21:50:36 +0200

    buscador

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
 import { LoginComponent } from '../login/login.component';
@@ -276,6 +276,9 @@ export class HomeComponent {
       label: "Melilla"
     }
   ]
-  
+
+  ngOnInit(){
+    
+  }
 
 }
